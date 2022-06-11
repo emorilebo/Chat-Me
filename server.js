4db149cf76fa3113fa787b1a173b4eebcd6b54e8
Godfrey <emorylebo@gmail.com>
2022-06-11 17:11:12 +0100

    ADD graphql and apollo client

---
+++ server.js
@@ -0,0 +1,14 @@
+import {ApolloServer, gql} from 'apollo-server'
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+  });
+  
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
