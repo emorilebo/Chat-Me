344d28eeb5ea8bf100d50e8c6328f13f6b3e1a3e
Godfrey <emorylebo@gmail.com>
2022-06-11 23:19:53 +0100

    UPDATE

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
 import {ApolloServer, gql} from 'apollo-server'
 
+const users = [
+    {
+        id: 1,
+        firstname: "Godfrey",
+        lastName: "Lebo",
+        email: "emorilebo@gmail.com",
+        password: "12345"
+
+    },
+    {
+        id: 2,
+        firstname: "Danny",
+        lastName: "Crane",
+        email: "dannycrane@gmail.com",
+        password: "123456"
+    }
+]
+
+const typeDefs = gql`
+    type Query{
+        # greet:String
+        users: [User]
+    }
+    type User{
+        id:ID
+        firstName:String
+        lastName:String
+        email:String
+    }
+`
+
+const resolvers = {
+    Query:{
+        // greet:()=>"Hello Emori"
+        users:()=>users
+    }
+}
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
