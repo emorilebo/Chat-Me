577aadb7e6b33ad752a625725973e605b83fe65f
Godfrey <emorylebo@gmail.com>
2022-06-13 15:08:46 +0100

    AuthScreen signup updated with login

--- client/src/pages/AuthScreen.js
+++ client/src/pages/AuthScreen.js
@@ -1,62 +1,70 @@
 import React, { useState } from "react";
-import { Box, Stack, Typography, Button, TextField } from "@mui/material";
+import { Box, Stack, Typography, Button, TextField, Card } from "@mui/material";
 
 const AuthScreen = () => {
+  const [showLogin, setShowLogin] = useState(true);
   const [formData, setFormData] = useState({});
 
   const handleChange = (e) => {
     setFormData({
-        ...formData,
+      ...formData,
       [e.target.name]: e.target.value,
     });
   };
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    console.log(formData)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <Box
-        component="form"
-        onSubmit={handleSubmit}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="80vh"
+      component="form"
+      onSubmit={handleSubmit}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
     >
-      <Stack
-        direction="column"
-        spacing={2}
-        sx={{width:"400px"}}
-      >
-        <Typography variant="h5">Please Signup</Typography>
-        <TextField
-          name="firstName"
-          label="First Name"
-          variant="standard"
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="lastName"
-          label="Last Name"
-          variant="standard"
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="email"
-          label="Email"
-          type="email"
-          variant="standard"
-          onChange={(e) => handleChange(e)}
-        />
-        <TextField
-          name="password"
-          type="password"
-          label="Password"
-          variant="standard"
-          onChange={(e) => handleChange(e)}
-        />
-        <Button variant="outlined" type="submit">Submit</Button>
-      </Stack>
+      <Card variant="outlined" sx={{ padding: "10px" }}>
+        <Stack direction="column" spacing={2} sx={{ width: "400px" }}>
+          <Typography variant="h5">
+            Please {showLogin ? "Login" : "Signup"}{" "}
+          </Typography>
+          {!showLogin && (
+            <>
+              <TextField
+                name="firstName"
+                label="First Name"
+                variant="standard"
+                onChange={(e) => handleChange(e)}
+              />
+              <TextField
+                name="lastName"
+                label="Last Name"
+                variant="standard"
+                onChange={(e) => handleChange(e)}
+              />
+            </>
+          )}
+
+          <TextField
+            name="email"
+            label="Email"
+            type="email"
+            variant="standard"
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Password"
+            variant="standard"
+            onChange={(e) => handleChange(e)}
+          />
+          <Button variant="outlined" type="submit">
+            {showLogin ? "Login" : "Signup"}
+          </Button>
+        </Stack>
+      </Card>
     </Box>
   );
 };
