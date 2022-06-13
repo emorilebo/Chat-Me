d6a15122debd651a6a44a50d448bde6a5a4ecad4
Godfrey <emorylebo@gmail.com>
2022-06-13 12:12:45 +0100

    ADD AuthScreen

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="">
-     
+     <h1>Hello</h1>
     </div>
   );
 }
---
+++ client/src/pages/AuthScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box, Stack, Typography, Button} from "@mui/material"
+
+const AuthScreen = () => {
+  return (
+    <Box>
+        
+    </Box>
+  )
+}
+
+export default AuthScreen
