881b5f2620f81595205cc6cdd833426ed0082a8d
Godfrey <emorylebo@gmail.com>
2022-06-13 15:45:21 +0100

    APP with Homescreen

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Divider, Typography } from '@mui/material'
+import UserCard from './UserCard'
 
 const Sidebar = () => {
+    const users = [
+        {id:1, firstName:"Godfrey", lastName:"Lebo"},
+        {id:2, firstName:"Francis", lastName:"Ekpan"},
+        {id:3, firstName:"Stella", lastName:"Thomas"},
+    ]
   return (
-    <div>Sidebar</div>
+    <Box>
+        <Typography variant="h6"> Raeven </Typography>
+        <Divider/>
+        {
+            users.map(item=>{
+                return <UserCard item={item}/>
+            })
+        }
+    </Box>
   )
 }
 
