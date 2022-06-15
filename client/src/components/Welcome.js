d6699a5efdb222e7d65a2524a0b9fb763d88667c
emorilebo <emorylebo@gmail.com>
2022-06-15 07:48:25 +0100

    UPDATE

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import { Stack, Typography } from '@mui/material'
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <Stack justifyContent="center" alignItems="center">
+      <Typography variant='h2'>Welcome to Raeven</Typography>
+    </Stack>
+  )
+}
+
+export default Welcome
--- client/src/pages/HomeScreen.js
+++ client/src/pages/HomeScreen.js
@@ -1,11 +1,23 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
+import Welcome from '../components/Welcome'
+
+const AllRoutes = ()=>{
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome/>}/>
+      <Route path="/:id/:name" element={<Welcome/>}/>
+    </Routes>
+  )
+}
 
 const HomeScreen = () => {
   return (
-    <Box>
+    <Box display="flex">
         <Sidebar/>
+        <AllRoutes/>
     </Box>
   )
 }
