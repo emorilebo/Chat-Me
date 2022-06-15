ded1319cf74879856536f22bf066a2931b9d8d52
emorilebo <emorylebo@gmail.com>
2022-06-15 08:07:22 +0100

    UPDATE

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,13 +1,23 @@
+import { AppBar, Avatar, Box, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const ChatScreen = () => {
-    const {id, name} = useParams()
-  return (
-    <div>
-      {id} {name}
-    </div>
-  )
+    const { id, name } = useParams()
+    return (
+        <Box flexGrow={1}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Avatar
+                        src={`https://avatars.dicebear.com/api/initials/${name}.svg`}
+                        sx={{ width: "32px", height: "32px" }}
+                    />
+                    <Typography variant='h6'>{name}</Typography>
+                </Toolbar>
+            </AppBar>
+
+        </Box>
+    )
 }
 
 export default ChatScreen
